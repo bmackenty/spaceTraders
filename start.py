89340d9d45e2ee3c34849ab7f4baa7951613b690
Bill MacKenty <bmackenty@users.noreply.github.com>
2024-09-10 09:23:44 +0200

    Update start.py

--- start.py
+++ start.py
@@ -3,6 +3,11 @@ os.system('clear')
 
 import space_system as ss
 import ship_templates as st
+import time
+
+# how many seconds to wait between updates
+cycleTime = 3
+cycleCount = 0
 
 # Create a new system
 currentSpace = ss.SpaceSystem()
@@ -13,3 +18,17 @@ currentSpace.add_object(ship)  # Add the ship to the system.
 
 # print the objects in the system
 currentSpace.show_objects()
+
+#create a "tick" function to update the system
+def tick(cycleTime, cycleCount):
+   
+    time.sleep(cycleTime)
+    currentSpace.update()
+    print("tick")
+    currentSpace.show_objects()
+
+# run the tick function
+while True:
+    cycleCount += 1
+    tick(cycleTime, cycleCount) # Update the system.
+    print(f"cycleCount: {cycleCount}")
